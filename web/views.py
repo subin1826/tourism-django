63cc2273b4a16257630f28d2d2f2bcc732cc81ed
subin1826 <subiniringal@gmail.com>
2025-01-13 18:14:55 +0530

    changes

--- web/views.py
+++ web/views.py
@@ -8,7 +8,9 @@ from django.contrib.auth.decorators import login_required
 
 
 
+
 # Create your views here.
+@login_required
 def home(request):
     return render(request,"home.html")
 def index(request):
